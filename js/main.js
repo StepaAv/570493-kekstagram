6950c1c5827456c9d7c2778be9780d0d6df1398d
StepaAv <353833@gmail.com>
2019-01-11 19:46:35 +0200

    on the path to glory

--- js/main.js
+++ js/main.js
@@ -1,73 +1,81 @@
-var getRandomNumber = function() {
-  var randomNumber = Math.floor(Math.random() * 25) + 1;
-  return randomNumber;
-}
-console.log(getRandomNumber() + ' - this is random number from 1 to 25');
+'use strict';
 
-var pictureUrl = 'photos/' + getRandomNumber() + '.jpg';
+var CONST_POSTS = 25;
+var MIN_LIKE = 15;
+var MAX_LIKE = 200;
+var MIN_AVATARS = 1;
+var MAX_AVATARS = 6;
 
-console.log(pictureUrl + ' - and this is random picture url');
-
-var getRandomLikes = function() {
-  var randomLikes = Math.floor(Math.random() * 200) + 15;
-  return randomLikes;
-}
-
-console.log(getRandomLikes() + ' - this is random quantity of likes');
-
-var commentsArray = ['Всё отлично!',
+var commentsArray = [
+  'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-console.log(commentsArray);
+var namesArray = [
+  'Liuda',
+  'xxxNAGIBATORxxx',
+  'ScrappyCoco78',
+  'Giuvento Manochi',
+  'Sania_3ato4ka',
+  'ActivateWindows',
+];
 
-var namesArray = ['Sania', 'Semion', 'Seriozha', 'Sara', 'Sofia', 'zapekanka'];
-
-console.log(namesArray);
-
-var randomName = namesArray[Math.floor(Math.random() * namesArray.length)];
-
-var oneItemArray = {
-  name: namesArray[Math.floor(Math.random() * namesArray.length)],
-  avatar: pictureUrl,
-  commment: commentsArray[Math.floor(Math.random() * commentsArray.length)]
+var getRandomNum = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
-console.log(oneItemArray);
+var getRandomElement = function (element) {
+  return element[Math.floor(Math.random() * element.length)];
+};
 
-var getRandomName = function() {
-  var randomName = namesArray[Math.floor(Math.random() * namesArray.length)];
-  return randomName;
-}
+var makeCommentString = function () {
+  var comment = '';
+  for (var i = 0; i < getRandomNum(1, 3); i++) {
+    comment += getRandomElement(commentsArray) + ' ';
+  }
+  return comment;
+};
 
-var getRandomComment = function() {
-  var randomComment = commentsArray[Math.floor(Math.random() * commentsArray.length)];
-  return randomComment;
-}
+console.log(makeCommentString());
 
-console.log(getRandomName());
-console.log(getRandomComment());
-
-var getRandomProfile = function() {
-
-  var threeItemArray = {
-    name: getRandomName(),
-    avatar: pictureUrl,
-    commment: getRandomComment()
+var makeComment = function () {
+  var commentObject = {
+    'avatar': 'img/avatar-' + getRandomNum(MIN_AVATARS, MAX_AVATARS + 1) + '.svg',
+    'message': makeCommentString(),
+    'name': getRandomElement(namesArray)
   };
-
-  return threeItemArray;
-}
-
-
-for (var i = 0; i <= 25; i++) {
-  getRandomComment();
-  console.log(getRandomProfile());
-  console.log('zapusk 25 profilej')
+  return commentObject;
 };
 
-var picturesBox = document.querySelector('.pictures');
+console.log(makeComment());
+
+var makeArrayComments = function () {
+  var arrayComments = [];
+
+  for (i = 0; i < getRandomNum(10, 19); i++) {
+    arrayComments.push(makeComment());
+  }
+
+  return arrayComments;
+};
+
+var addPhotoPost = function (number) {
+  var post = {
+    'url': 'photos/' + (number + 1) + '.jpg',
+    'likes': getRandomNum(MIN_LIKE, MAX_LIKE),
+    'comments': makeArrayComments()
+  };
+  return post;
+};
+
+var makeArrayPosts = function () {
+  var arrayPosts = [];
+  for (var i = 0; i < CONST_POSTS; i++) {
+    arrayPosts.push(addPhotoPost(i));
+  }
+  return arrayPosts;
+};
