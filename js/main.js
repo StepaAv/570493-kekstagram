54e876c662b891756ed1c4eed163d4d8d5e0a32b
StepaAv <353833@gmail.com>
2019-01-17 22:02:23 +0200

    pc transfer

--- js/main.js
+++ js/main.js
@@ -31,15 +31,15 @@ var namesArray = [
   'ActivateWindows',
 ];
 
-var getRandomNum = function (min, max) {
+var getRandomNum = function(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-var getRandomSegment = function (element) {
+var getRandomSegment = function(element) {
   return element[Math.floor(Math.random() * element.length)];
 };
 
-var makeCommentString = function () {
+var makeCommentString = function() {
   var comment = '';
   for (var i = 0; i < getRandomNum(MIN_COMMENTS_STRING, MAX_COMMENTS_STRING); i++) {
     comment += getRandomSegment(commentsArray) + ' ';
@@ -47,7 +47,7 @@ var makeCommentString = function () {
   return comment;
 };
 
-var constructOneComment = function () {
+var constructOneComment = function() {
   var commentObject = {
     avatar: 'img/avatar-' + getRandomNum(MIN_AVATARS, MAX_AVATARS + 1) + '.svg',
     message: makeCommentString(),
@@ -56,7 +56,7 @@ var constructOneComment = function () {
   return commentObject;
 };
 
-var makeArrayOfComments = function () {
+var makeArrayOfComments = function() {
   var arrayComments = [];
   for (var i = 0; i < getRandomNum(MIN_COMMENTS_ARRAY, MAX_COMMENTS_ARRAY); i++) {
     arrayComments.push(constructOneComment());
@@ -65,7 +65,7 @@ var makeArrayOfComments = function () {
   return arrayComments;
 };
 
-var addPostPhoto = function (number) {
+var addPostPhoto = function(number) {
   var post = {
     url: 'photos/' + (number + 1) + '.jpg',
     likes: getRandomNum(MIN_LIKE, MAX_LIKE),
@@ -74,7 +74,7 @@ var addPostPhoto = function (number) {
   return post;
 };
 
-var makePostsArray = function () {
+var makePostsArray = function() {
   var arrayPosts = [];
   for (var i = 0; i < CONST_POSTS; i++) {
     arrayPosts.push(addPostPhoto(i));
@@ -82,7 +82,7 @@ var makePostsArray = function () {
   return arrayPosts;
 };
 
-var assemblingPostPhoto = function (photo) {
+var assemblingPostPhoto = function(photo) {
   var postPhotoElement = templateSection.cloneNode(true);
   postPhotoElement.querySelector('.picture__img').src = photo.url;
   postPhotoElement.querySelector('.picture__likes').textContent = photo.likes;
@@ -91,8 +91,7 @@ var assemblingPostPhoto = function (photo) {
   return postPhotoElement;
 };
 
-
-var initPostPhotoArray = function () {
+var initPostPhotoArray = function() {
   var makePosts = makePostsArray();
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < CONST_POSTS; i++) {
@@ -107,63 +106,111 @@ initPostPhotoArray();
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 
+// @oldfox
+var uploadInput = document.querySelector('#upload-file');
+
 var uploadPhotoOpen = document.getElementById('upload-file');
 var uploadPhotoClose = document.getElementById('upload-cancel');
 var uploadPhotoForm = document.querySelector('.img-upload__overlay');
 
-var onPopupEscPress = function (evt) {
+var onPopupEscPress = function(evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     closeUpload();
   }
 };
 
+// @oldfox
+// открываем форму после того как выбрали изображение
+var onUploadInputChange = function(e) {
+  openUpload();
+};
+
+uploadInput.addEventListener('change', onUploadInputChange);
+
 // ubirajet pojavlenije okna zagruzki foto
+/*
 uploadPhotoOpen.addEventListener('click', function (evt) {
   evt.preventDefault();
 });
 
-var openUpload = function () {
+// ⚠️ preventDefault можно было делать в том же обработчике что и выполение открытия окна
+// но в любом случае это нам не нужно используем change открываем окно только после выбора фотки пользователем
+// см. выше стр.122
+uploadPhotoOpen.addEventListener('click', function(evt) {
+   evt.preventDefault();
+  openUpload();
+});
+*/
+
+var openUpload = function() {
   uploadPhotoForm.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
 };
 
-var closeUpload = function () {
+var closeUpload = function() {
   uploadPhotoForm.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
-uploadPhotoOpen.addEventListener('click', function () {
+/* ненужно
+uploadPhotoOpen.addEventListener('click', function() {
   openUpload();
 });
+*/
 
-uploadPhotoClose.addEventListener('click', function () {
+uploadPhotoClose.addEventListener('click', function() {
   closeUpload();
 });
 
-uploadPhotoClose.addEventListener('keydown', function (evt) {
+uploadPhotoClose.addEventListener('keydown', function(evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     closeUpload();
   }
 });
+
+
 
 var zoomPhotoPlus = document.querySelector('.scale__control--bigger');
 var zoomPhotoMinus = document.querySelector('.scale__control--smaller');
 var zoomPhotoValue = document.querySelector('.scale__control--value');
 var imageScale = document.querySelector('.img-upload__preview');
-var valueStepPercent = '25';
-var valueStepScale = '0.25';
 
-imageScale.style.transform = 'scale(0.75)';
+imageScale.style.transform = 'scale(1)';
 zoomPhotoValue.value = '100%';
 
-var removingPhotoZoom = function () {
-  zoomPhotoValue.value = zoomPhotoValue.value - valueStepPercent;
-  imageScale.style.transform = imageScale.style.transform - valueStepScale;
+var IMG_SCALE_STEP = 25;
+var IMG_MAX_SCALE = 100;
+var IMG_MIN_SCALE = 25;
+var imgScale = 100;
+
+var plusImgScale = function() {
+  var scale = imgScale + IMG_SCALE_STEP;
+  if (scale > IMG_MAX_SCALE) {
+    scale = IMG_MAX_SCALE;
+  }
+
+
+  imgScale = scale;
+  zoomPhotoValue.value = scale + '%';
+  imageScale.style.transform = 'scale(' + scale / 100 + ')';
+};
+
+zoomPhotoPlus.addEventListener('click', plusImgScale);
+
+
+
+var minusImgScale = function() {
+  var scale = imgScale - IMG_SCALE_STEP;
+  if (scale < IMG_SCALE_STEP) {
+    scale = IMG_MIN_SCALE;
+  }
+
+
+  imgScale = scale;
+  zoomPhotoValue.value = scale + '%';
+  imageScale.style.transform = 'scale(' + scale / 100 + ')';
 };
 
 
-zoomPhotoMinus.addEventListener('click', function () {
-  if (zoomPhotoValue.value >= '50%') {
-    removingPhotoZoom();
-  }
-});
+
+zoomPhotoMinus.addEventListener('click', minusImgScale);
