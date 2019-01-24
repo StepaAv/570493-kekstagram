77b7f540ff32b6ad1800709440d434b955af7aee
StepaAv <353833@gmail.com>
2019-01-24 11:58:50 +0200

    I'm potatoe

--- js/solopic.js
+++ js/solopic.js
@@ -1,42 +1,123 @@
 'use strict';
 (function () {
+
+  var AVATAR_WIDTH = 35;
+  var AVATAR_HEIGHT = 35;
+  var RENDER_COMMENTS_COUNT = 5;
+
   var bigPicture = document.querySelector('.big-picture');
+  var bigPictureClose = document.querySelector('.big-picture__cancel');
 
-  var renderComment = function (comment) {
-    var commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
-    var commentElement = commentTemplate.cloneNode(true);
+  var image = document.querySelector('.big-picture__img img');
+  var pictureLikes = document.querySelector('.likes-count');
+  var pictureDescription = document.querySelector('.social__caption');
+  var commentsList = bigPicture.querySelector('.social__comments');
+  var commentsCounter = document.querySelector('.social__comment-count');
+  var pictureComments = document.querySelector('.social__comments');
+  var commentsLoaderButton = document.querySelector('.social__comments-loader');
 
-    commentElement.querySelector('.social__picture').src = comment.avatar;
-    commentElement.querySelector('.social__text').textContent = comment.message;
 
-    return commentElement;
-  };
+  var pressEscButtonHandler = null;
+  var renderedCommentsCount = 0;
+  var displayedPhotoData = null;
 
-  var showComment = function (bigPictureItem) {
-    var commentsList = bigPicture.querySelector('.social__comments');
-    var commentItem = commentsList.querySelectorAll('.social__comment');
-    commentItem.forEach(function (item) {
-      commentsList.removeChild(item);
-    });
+  var setEscHandler = function (handler) {
+    pressEscButtonHandler = handler(closePhoto);
+  }
+
+  var closePhoto = function () {
+    bigPicture.classList.add('hidden');
+    renderedCommentsCount = 0;
+
+    bigPictureClose.removeEventListener('click', picCloseBtn);
+    document.removeEventListener('keydown', pressEscButtonHandler);
+    // window.filters.activate();
+  }
+
+  var picCloseBtn = function (event) {
+    event.preventDefault();
+    closePhoto();
+  }
+
+
+
+  var updateCommentsCount = function (totalComments, renderedComments) {
+    commentsCounter.innerHTML = '';
+    var rendered = document.createTextNode(renderedComments + ' из ');
+    var total = document.createElement('span');
+    total.textContent = totalComments;
+    total.classList.add('comments-count');
+    commentsCounter.appendChild(rendered);
+    commentsCounter.appendChild(total);
+  }
+
+  var createComment = function (count, comments) {
     var fragment = document.createDocumentFragment();
-    bigPictureItem.comments.forEach(function (item) {
-      fragment.appendChild(renderComment(item));
-    });
-    commentsList.appendChild(fragment);
-  };
+    count += renderedCommentsCount;
+    for (var i = renderedCommentsCount; i < count; i++) {
+      if (!comments[i]) {
+        commentsLoaderButton.classList.add('hidden');
+        break;
+      }
+      var socialComment = document.createElement('li');
+      socialComment.classList.add('social__comment');
+
+      var avatar = document.createElement('img');
+      avatar.classList.add('social__picture');
+      avatar.src = comments[i].avatar;
+      avatar.title = comments[i].name;
+      avatar.alt = 'Аватар комментатора фотографии';
+      avatar.width = AVATAR_WIDTH;
+      avatar.height = AVATAR_HEIGHT;
+      socialComment.appendChild(avatar);
+
+      var socialText = document.createElement('p');
+      socialText.classList.add('social__text');
+      socialText.textContent = comments[i].message;
+      socialComment.appendChild(socialText);
+      pictureComments.appendChild(socialComment);
+      renderedCommentsCount++;
+    }
+    pictureComments.appendChild(fragment);
+  }
+
+
+  var openPhoto = function (photo) {
+    image.src = photo.url;
+    pictureLikes.textContent = photo.likes;
+    pictureDescription.textContent = photo.description;
+    displayedPhotoData = photo;
+    pictureComments.innerHTML = '';
+
+    createComment(RENDER_COMMENTS_COUNT, photo.comments);
+
+    bigPictureClose.addEventListener('click', picCloseBtn);
+    document.addEventListener('keydown', pressEscButtonHandler);
+
+    bigPicture.classList.remove('hidden');
+    // window.filters.deactivate();
+  }
+
+
+  // delete default comentariji
+  // pictureComments.innerHTML = '';
 
   var showBigPicture = function (bigPictureItem) {
     bigPicture.classList.remove('hidden');
-
-    bigPicture.querySelector('.big-picture__img img').src = bigPictureItem.url;
-    bigPicture.querySelector('.likes-count').textContent = bigPictureItem.likes;
-    bigPicture.querySelector('.comments-count').textContent = bigPictureItem.comments.length;
-
-    showComment(bigPictureItem);
-
     return bigPicture;
   };
 
+  // zakrytije fotochki bolshoj kotoraja
+  var closingBigPicture = function () {
+    bigPicture.classList.add('hidden');
+  };
+  bigPictureClose.addEventListener('click', closingBigPicture);
+  document.addEventListener('keydown', function (evt) {
+
+    if (window.util.isEscKeycode(evt)) {
+      closingBigPicture();
+    }
+  });
   window.solopic = {
     bigPicture: bigPicture,
     showBigPicture: showBigPicture
